67da6dcb48ec42696e8be1b7e31a19962b00f649
Matheus <matheus_fellip@hotmail.com>
2020-01-17 12:25:10 -0300

    dev update

--- src/Controllers/SearchController.js
+++ src/Controllers/SearchController.js
@@ -6,10 +6,23 @@ module.exports = {
     async index(request, response) {
         const { latitude, longitude, techs} = request.query;
 
-        // const techsArray = ParseStringAsArray(techs);      
+        const techsArray = techs.split(',').map(tech => tech.trim());    
+
+        const devs = await Dev.find({
+            techs: {
+                $in: techsArray,
+            },
+            location:{
+                $near: {
+                    type: 'Point',
+                    coordinates: [longitude, latitude],               
+                },
+                $maxDistance: 10000
+            },
+        });
         
 
-        return response.json({devs: []})
+        return response.json({ devs })
 
     },
 }
