ffa57c8bb3abe6ce63a4b2efa83fc206e5cd16c4
Matheus <matheus_fellip@hotmail.com>
2020-01-22 16:24:14 -0300

    dev update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const cors = require('cors');
 
 const app = express();
 
@@ -9,6 +10,7 @@ mongoose.connect('mongodb+srv://matheus:matheus@cluster0-pg5bz.mongodb.net/week1
     useUnifiedTopology: true
 });
 
+app.use(cors())
 app.use(express.json());
 app.use(routes);
 
